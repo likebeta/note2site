a50e809ceba05fc7d1c6019977f558e0dcffad0d
likebeta <ixxoo.me@gmail.com>
2013-05-28 07:53:39 -0700

    modify

--- oauth/index.php
+++ oauth/index.php
@@ -28,20 +28,40 @@ require('ynote_parse.php');
 	$user_info_response = $client->getUserInfo($oauth_access_token, $oauth_access_secret);
 	echo 'userinfo:<br />';printUserInfo(parseUserInfo($user_info_response));
 */
-$list_notebooks_response = $client->listNotebooks($oauth_access_token, $oauth_access_secret);
+/*$list_notebooks_response = $client->listNotebooks($oauth_access_token, $oauth_access_secret);
 $notebooks = parseNotebooks($list_notebooks_response);
 foreach ($notebooks as $notebook){
 	echo $notebook->name.' has '.$notebook->notes_num.' notes:<br />';
 	$list_notes_response = $client->listNotes($oauth_access_token, $oauth_access_secret, $notebook->path);
 	$notes = parseNotes($list_notes_response);
+	$notes = array('/V5_B_BU/0175E4E030CF431CAAD60AC703B4B146','/8E00330A442B49688B2ECEFE3E9A7FBA/55E1FFBB77574591B51A17FCAC0CA874');
 	foreach($notes as $notePath){
 		$get_note_response = $client->getNote($oauth_access_token, $oauth_access_secret, $notePath);
 		$note = parseNote($get_note_response);
-		echo $note->title.'<br />';
+		echo $note->title.':'.$note->path.'<br />';
 		write_file($note->title,$note->content);
 	}
 	echo '<br />';
 }
+*/
+	$notes = array('/V5_B_BU/0175E4E030CF431CAAD60AC703B4B146','/8E00330A442B49688B2ECEFE3E9A7FBA/55E1FFBB77574591B51A17FCAC0CA874');
+	foreach($notes as $notePath){
+		$get_note_response = $client->getNote($oauth_access_token, $oauth_access_secret, $notePath);
+		$note = parseNote($get_note_response);
+		echo $note->title.':'.$note->path.'<br />';
+		preg_match_all('<img.*?\s+src=\"(.+?)\".*?(data-media-type=\"image\")?>',$note->content,$out);
+		$imgs = array();
+		foreach($out[1] as $img){
+			$imgurl = $client->getAuthorizedDownloadLink($oauth_access_token, $oauth_access_secret, $img);
+			$imgs[] = $imgurl;
+		}
+		preg_match_all('<img.*?\s+src=\"(.+?)\".+?\s+path=\"(.+?)\".*?(data-media-type=\"attachment\").*?>',$note->content,$out);
+		var_dump($out);
+		echo '<br />';
+		write_file($note->title,$note->content);
+	}
+	echo '<br />';
+
 /*
 	// listNotebooks
     echo '<br /><br />';
