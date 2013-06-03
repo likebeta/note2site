e8745b62c0325a37c3f0d1b2538a732a0c034218
likebeta <ixxoo.me@gmail.com>
2013-06-02 21:35:24 -0400

    modify

--- config.php
+++ config.php
@@ -1,9 +1,8 @@
 <?php
-$site_url = 'http://localhost/note2site';
+$site_url = 'http://www.ibiu.org';
 $base_url = 'http://note.youdao.com';
 $oauth_consumer_key = "780b8bb560897a357c78de3e85b88cd9";
 $oauth_consumer_secret = "82c911c61bb3067058febaea94007a10";
 $oauth_access_token = 'ec4087f1c03661637bc148f1f4a665bb';
 $oauth_access_secret = '598301bc3f0e4187bcb0b26482a06eb1';
-
 ?>
