c1f765ddde66c5790a0d6b87a33876332de1aeb0
likebeta <ixxoo.me@gmail.com>
2013-06-25 00:28:33 -0400

    modify config

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 $site_url = '';
 
-// note相关
+// note
 $base_url = 'http://note.youdao.com';
 $oauth_consumer_key = '780b8bb560897a357c78de3e85b88cd9';
 $oauth_consumer_secret = '82c911c61bb3067058febaea94007a10';
 $oauth_access_token = 'ec4087f1c03661637bc148f1f4a665bb';
 $oauth_access_secret = '598301bc3f0e4187bcb0b26482a06eb1';
 
-$flush_token = 'r4c0oifbhvat3dszm9nuyl6qpj5e2x7wkg81';// 刷新需要的token
+$flush_token = 'r4c0oifbhvat3dszm9nuyl6qpj5e2x7wkg81';// token of flush 
 
-// db相关
+// db
 $db_host = '';
 $db_name = '';
 $db_username = '';
